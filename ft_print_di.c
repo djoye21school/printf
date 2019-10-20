f91ff1ee9331248adb7e865f3bc88133f13c06f7
Shrimp Doughnut <sdoughnu@at-h2.21-school.ru>
2019-10-20 15:16:53 +0300

    ura, parser pochti rabotaet

--- ft_print_di.c
+++ ft_print_di.c
@@ -14,9 +14,10 @@
 
 int		ft_print_di(va_list *ap, t_flags *yep, long long int res)
 {
-	printf("\nn%d\n", yep->plus);
+	printf("\n%d\n", yep->plus);
 	printf("\n%d\n", yep->len);
-
+    printf("\n%lld\n", yep->prcn);
 	printf("\n%d\n", yep->min);
+    printf("\n%lld\n", yep->width);
 	return (ft_putstr(ft_itoa_base(res, 10, 'a', 1)));
 }
