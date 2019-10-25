3892170b20727573f2d45c02ddf916c5dd166d39
djoye21school <55892630+djoye21school@users.noreply.github.com>
2019-10-25 20:27:26 +0300

    Add files via upload

--- float.c
+++ float.c
@@ -6,6 +6,7 @@
 /*   By: djoye <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 18:05:34 by djoye             #+#    #+#             */
+/*   Updated: 2019/10/25 20:26:10 by djoye            ###   ########.fr       */
 /*   Updated: 2019/10/25 16:03:25 by djoye            ###   ########.fr       */
 /*   Updated: 2019/10/25 15:44:02 by djoye            ###   ########.fr       */
 /*                                                                            */
@@ -13,18 +14,18 @@
 
 #include "ft_printf.h"
 
-char			*ft_itoa_base(long long dig, int base, char a)
+char					*ft_itoa_base(long long dig, int base, char a)
 {
 	char				*s;
 	int					i;
 	unsigned long long	nb;
 	int					res;
-	int                 sgn;
+	int					sgn;
 
 	sgn = (dig >= 0) ? 1 : -1;
 	i = (dig < 0 && base == 10) || dig == 0 ? 1 : 0;
-    dig = (sgn >= 0) ? dig : dig * (-1);
-    nb = dig;
+	dig = (sgn >= 0) ? dig : -dig;
+	nb = dig;
 	while (dig && ++i && base > 1 && base <= 36)
 		dig = dig / base;
 	if (!(s = (char*)malloc(sizeof(char) * (i + 1))))
@@ -40,7 +41,7 @@ char			*ft_itoa_base(long long dig, int base, char a)
 	return (s);
 }
 
-char				*ft_float(long double nb, int acc)
+char					*ft_float(long double nb, int acc)
 {
 	char				*num;
 	unsigned long long	integer;
@@ -49,6 +50,8 @@ char				*ft_float(long double nb, int acc)
 	long double			ld;
 	char				*res;
 
+	if (nb != nb)
+		return ("nan");
 	sgn = nb < 0 ? -1 : 1;
 	nb = sgn < 0 ? -nb : nb;
 	i = 0;
@@ -62,7 +65,6 @@ char				*ft_float(long double nb, int acc)
 	num = ft_itoa_base(integer, 10, 'A');
 	res[i + acc + 1] = '\0';
 	i = 0;
-	if (sgn < 0)
 	if ( sgn < 0)
 	{
 		res[i] = '-';
@@ -70,20 +72,24 @@ char				*ft_float(long double nb, int acc)
 	}
 	while (*num)
 		res[i++] = *num++;
-	res[i++] = acc != 0 ? '.' : '\0';
-	nb = nb - integer;
-	sgn = acc;
-	while (sgn-- && (nb = nb * 10) > 0)
-		if (nb < 1)
-			res[i++] = '0';
-	integer = (unsigned long long int)nb;
-	integer += (nb - integer) >= 0.5 ? 1 : 0;
-	num = ft_itoa_base(integer, 10, 'A');
-	if (num[0] != '0' && acc)
-		while (*num)
-			res[i++] = *num++;
-	else
-		while (acc-- > 0)
-			res[i++] = '0';
+	res[i] = acc != 0 ? '.' : '\0';
+	while (acc-- > 0 && (nb = nb * 10) >= 0 && ++i)
+	{
+		if ((nb - (unsigned long long)nb) >= 0.5 && acc == 0)
+			res[i] = (unsigned long long)nb % 10 + 1 + '0';
+		else res[i] = ((unsigned long long)nb % 10 + '0');
+		nb = nb - (unsigned long long)nb;
+	}
 	return (res);
+}
+
+int						main(void)
+{
+	long double nb;
+
+	nb = (long double)345.345535435;
+	printf("%llu\n", (unsigned long long)nb);
+	printf("%s", ft_float(nb, 69));
+	printf("\n%.69Lf", nb);
+	return (0);
 }
