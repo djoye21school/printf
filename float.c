808177353de02de561d8295860eca9f9a691def6
Shrimp Doughnut <sdoughnu@ox-k2.21-school.ru>
2019-10-27 14:19:14 +0300

    d

--- float.c
+++ float.c
@@ -14,31 +14,31 @@
 
 #include "ft_printf.h"
 
-char					*ft_itoa_base(long long dig, int base, char a)
+char 	*ft_itoa_base(long long dig, int base, char a)
 {
-    char				*s;
-    int					i;
-    unsigned long long	nb;
-    int					res;
-    int					sgn;
+	char				*s;
+	int					i;
+	unsigned long long	nb;
+	int					res;
+	int					sgn;
 
-    sgn = (dig >= 0) ? 1 : -1;
-    i = (dig < 0 && base == 10) || dig == 0 ? 1 : 0;
-    dig = (sgn >= 0) ? dig : -dig;
-    nb = dig;
-    while (dig && ++i && base > 1 && base <= 36)
-        dig = dig / base;
-    if (!(s = (char*)malloc(sizeof(char) * (i + 1))))
-        return (NULL);
-    s[i--] = '\0';
-    s[0] = sgn < 0 && base == 10 ? '-' : '0';
-    while (base > 1 && base <= 36 && nb)
-    {
-        res = nb % base;
-        s[i--] = res <= 9 ? res + '0' : res + a - 10;
-        nb = nb / base;
-    }
-    return (s);
+	sgn = (dig >= 0) ? 1 : -1;
+	i = (dig < 0 && base == 10) || dig == 0 ? 1 : 0;
+	dig = (sgn >= 0) ? dig : -dig;
+	nb = dig;
+	while (dig && ++i && base > 1 && base <= 36)
+		dig = dig / base;
+	if (!(s = (char*)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	s[i--] = '\0';
+	s[0] = sgn < 0 && base == 10 ? '-' : '0';
+	while (base > 1 && base <= 36 && nb)
+	{
+		res = nb % base;
+		s[i--] = res <= 9 ? res + '0' : res + a - 10;
+		nb = nb / base;
+	}
+	return (s);
 }
 
 char					*ft_float(long double nb, int acc)
